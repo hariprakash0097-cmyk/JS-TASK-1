8c19243c0bceb8ce574210fc5bcbfa490e73f78a
Hariprakash Kumar <hariprakash0097@gmail.com>
2026-03-11 15:30:44 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,99 @@
+// ....................Task-1....................
+var age = 26;
+console.log(age);
+age = 27;
+console.log(age);
+
+// ....................Task-2....................
+let myName = "Hariprakash";
+console.log(myName);
+myName = "Hari_Prakash";
+console.log(myName);
+
+// ....................Task-3....................
+const birthYear = 2000;
+console.log(birthYear);
+
+// ....................Task-4....................
+var a = 10;
+console.log(a);
+var a = 20;
+console.log(a);
+
+// ....................Task-5....................
+let Msg = "Hello";
+// let Msg = "Hi"; // Error check (Commented)
+
+// ....................Task-6....................
+console.log("Hariprakash");
+console.log(26);
+console.log("Tirupur");
+console.log(100);
+console.log("JavaScript is fun");
+
+// ....................Task-7....................
+alert("Welcome to JavaScript");
+
+// ....................Task-8....................
+var likesCoding = confirm("Do you like coding?");
+console.log(likesCoding);
+
+// ....................Task-9....................
+var inputName = prompt("What is your name?");
+console.log(inputName);
+
+// ....................Task-10 to 12....................
+console.warn("This is a warning");
+console.error("This is an error");
+// console.clear();
+
+// ....................Task-13 to 16....................
+document.writeln("Name: " + inputName + "<br>");
+let user_Name = prompt("Enter Name (Task 14):");
+alert(user_Name);
+console.log(user_Name);
+
+var user_Age = prompt("Enter Age:");
+console.log(user_Age);
+document.writeln("Age: " + user_Age + "<br>");
+
+const breakfast = confirm("Have you had breakfast?");
+console.log(breakfast);
+
+// ....................Task-17 to 19....................
+var city = "Chennai";
+let country = "India";
+const planet = "Earth";
+console.log(city, country, planet);
+
+let val = 5;
+console.log(val);
+val = 10; console.log(val);
+val = 15; console.log(val);
+
+// ....................Task-20 to 26....................
+const uName = prompt("Task 20 - Name:");
+let uAge = prompt("Task 20 - Age:");
+var isStudent = confirm("Are you a student?");
+console.log(uName, uAge, isStudent);
+alert("Welcome " + uName);
+
+const task25Name = prompt("Task 25 - Enter Name:");
+document.writeln("Your name is: " + task25Name + "<br>");
+console.log("Your name is: " + task25Name);
+
+// ....................Task-28 to 30....................
+if(confirm("Are you 18+?")) {
+    console.log("Adult");
+} else {
+    console.warn("Minor");
+}
+
+var fruit = "Apple";
+fruit = "Banana";
+let vehicle = "Car";
+vehicle = "Bike";
+console.log(fruit, vehicle);
+
+alert("Practice End");
+document.writeln("Thank You");
